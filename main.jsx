2d10c676d63787d7f2df1c46ba0e464d9bd3230f
kcecapital-dot <investwithkce@gmail.com>
2026-04-06 23:09:16 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import KCEHub from './kce-hub.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KCEHub />
-  </React.StrictMode>
-)
